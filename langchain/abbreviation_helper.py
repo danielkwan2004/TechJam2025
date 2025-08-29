a58d64e622374b84b2028c6d61aea101183fc077
Shaun Tan <shauntansr@gmail.com>
2025-08-29 23:36:04 +0800

    Change to using supabase as storage for abbreviations

--- langchain/abbreviation_helper.py
+++ langchain/abbreviation_helper.py
@@ -1,21 +1,31 @@
-import json
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
 
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 # Helper function that takes in a query, then outputs a list of abbreviations used.
 # Duplicates will be added.
 
-filepath = '../knowledge_base/abbreviations.json'
 default_doc = "Hello World."
 
 def retrieve_abbreviations(doc=default_doc):
-    abbreviations = json.load(open(filepath))
+    abbreviations = (
+        supabase.table("abbreviations")
+        .select("*")
+        .execute()
+    ).data
     out = []
 
     for abbreviation in abbreviations:
         if abbreviation['term'] in doc:
-            out.append(abbreviation)
+            out.append({k: v for k, v in abbreviation.items() if k != "id"})
 
     return out
 
 if __name__ == '__main__':
-    abbr = retrieve_abbreviations()
+    abbr = retrieve_abbreviations('Notifications will be tailored by age using ASL, allowing us to throttle or suppress push alerts for minors. EchoTrace will log adjustments, and CDS will verify enforcement across rollout waves.')
     print(abbr)
